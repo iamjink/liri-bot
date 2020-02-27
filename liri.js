ff72e3fb031e4c5a926bd12c9160e001e5255a62
iamjink <jinijinjin@gmail.com>
2020-02-27 04:11:39 -0800

    fixing movie-this with default movie name

--- liri.js
+++ liri.js
@@ -11,7 +11,7 @@ var fs = require("fs");
 //     id: keys.spotify.id,
 //     secret: keys.spotify.secrets
 // });
-var defMovie = "Mr.Nobody";
+var defaultMovie = "Mr.Nobody";
 var defSong = "The Sign";
 var defaultBand = "Metallica";
 
@@ -38,34 +38,34 @@ switch (action) {
 }
 
 function bands(action) {
-    if (bandname == null) {
+    if (bandname === null) {
         var bandname = defaultBand;
         axios.get("https://rest.bandsintown.com/artists/" + bandname + "/events?app_id=codingbootcamp").then(
-            function (response) {
-                result = `
+                function (response) {
+                    result = `
             Name of the band is: ${defaultBand}
             Name of the venue is: ${response.data[0].venue.name}
             Venue location is: ${response.data[0].venue.city}, ${response.data[0].venue.region}, ${response.data[0].venue.country}
             Date of the event is: ${moment(response.data[0].datetime).format('MM/DD/YYYY')}
             `
-                console.log(result);
+                    console.log(result);
 
-            })
-        .catch(function (error) {
-            if (error.response) {
-                console.log("---------------Data---------------");
-                console.log(error.response.data);
-                console.log("---------------Status---------------");
-                console.log(error.response.status);
-                console.log("---------------Status---------------");
-                console.log(error.response.headers);
-            } else if (error.request) {
-                console.log(error.request);
-            } else {
-                console.log("Error", error.message);
-            }
-            console.log(error.config);
-        });
+                })
+            .catch(function (error) {
+                if (error.response) {
+                    console.log("---------------Data---------------");
+                    console.log(error.response.data);
+                    console.log("---------------Status---------------");
+                    console.log(error.response.status);
+                    console.log("---------------Status---------------");
+                    console.log(error.response.headers);
+                } else if (error.request) {
+                    console.log(error.request);
+                } else {
+                    console.log("Error", error.message);
+                }
+                console.log(error.config);
+            });
     } else {
         var bandname = get.split(" ").join("+");
         var queryUrl = "https://rest.bandsintown.com/artists/" + bandname + "/events?app_id=codingbootcamp";
@@ -102,42 +102,82 @@ function bands(action) {
 
 }
 
-// function songs() {
+function songs() {
 
-// }
+}
 
 function movies(get) {
+    if (moviename === null) {
+        var moviename = defaultMovie;
+        var queryUrl = "http://www.omdbapi.com/?t=" + moviename + "&y=&plot=short&apikey=trilogy";
+        console.log(queryUrl);
 
-    var moviename = get.split(" ").join("+");s
-    var queryUrl = "http://www.omdbapi.com/?t=" + moviename + "&y=&plot=short&apikey=trilogy";
-    console.log(queryUrl);
-
-    axios.get("http://www.omdbapi.com/?t=" + moviename + "&y=&plot=short&apikey=trilogy").then(
-            function (response) {
-                console.log("The movie's Title is: " + response.data.Title);
-                console.log("The movie's Year is: " + response.data.Year);
-                console.log("The movie's imbdbRating is: " + response.data.imbdbRating);
-                console.log("The movie's Rotten Tomatoes Rating is: " + response.data.Ratings[2].Value);
-                console.log("The movie's Country is: " + response.data.Country);
-                console.log("The movie's language is: " + response.data.Language);
-                console.log("The movie's plot is: " + response.data.Plot);
-                console.log("The movie's actors is: " + response.data.Actors);
+        axios.get("http://www.omdbapi.com/?t=" + moviename + "&y=&plot=short&apikey=trilogy").then(
+                function (response) {
+                    var result = `
+                The movie's Title is: ${response.data.Title}
+                The movie's Year is: ${response.data.Year}
+                The movie's imbdbRating is: ${response.data.imbdbRating}
+                The movie's Rotten Tomatoes Rating is: ${response.data.Ratings[2].Value}
+                The movie's Country is: ${response.data.Country}
+                The movie's language is: ${response.data.Language}
+                The movie's plot is: ${response.data.Plot}
+                The movie's actors is: ${response.data.Actors}
+                `
+                    console.log(result);
+                })
+            .catch(function (error) {
+                if (error.response) {
+                    console.log("---------------Data---------------");
+                    console.log(error.response.data);
+                    console.log("---------------Status---------------");
+                    console.log(error.response.status);
+                    console.log("---------------Status---------------");
+                    console.log(error.response.headers);
+                } else if (error.request) {
+                    console.log(error.request);
+                } else {
+                    console.log("Error", error.message);
+                }
+                console.log(error.config);
             })
-        .catch(function (error) {
-            if (error.response) {
-                console.log("---------------Data---------------");
-                console.log(error.response.data);
-                console.log("---------------Status---------------");
-                console.log(error.response.status);
-                console.log("---------------Status---------------");
-                console.log(error.response.headers);
-            } else if (error.request) {
-                console.log(error.request);
-            } else {
-                console.log("Error", error.message);
-            }
-            console.log(error.config);
-        });
+    } else {
+
+        var moviename = get.split(" ").join("+");
+
+        var queryUrl = "http://www.omdbapi.com/?t=" + moviename + "&y=&plot=short&apikey=trilogy";
+        console.log(queryUrl);
+
+        axios.get("http://www.omdbapi.com/?t=" + moviename + "&y=&plot=short&apikey=trilogy").then(
+                function (response) {
+                    var result = `
+                The movie's Title is: ${response.data.Title}
+                The movie's Year is: ${response.data.Year}
+                The movie's imbdbRating is: ${response.data.imbdbRating}
+                The movie's Rotten Tomatoes Rating is: ${response.data.Ratings[2].Value}
+                The movie's Country is: ${response.data.Country}
+                The movie's language is: ${response.data.Language}
+                The movie's plot is: ${response.data.Plot}
+                The movie's actors is: ${response.data.Actors}
+                `
+                    console.log(result);
+                })
+            .catch(function (error) {
+                if (error.response) {
+                    console.log("---------------Data---------------");
+                    console.log(error.response.data);
+                    console.log("---------------Status---------------");
+                    console.log(error.response.status);
+                    console.log("---------------Status---------------");
+                    console.log(error.response.headers);
+                } else if (error.request) {
+                    console.log(error.request);
+                } else {
+                    console.log("Error", error.message);
+                }
+                console.log(error.config);
+            })
+    };
 
 };
 
