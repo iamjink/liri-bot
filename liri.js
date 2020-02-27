67edcfbe4bc6a95afe8bd36754468c60c8a4f71c
iamjink <jinijinjin@gmail.com>
2020-02-27 03:59:15 -0800

    adding function bands

--- liri.js
+++ liri.js
@@ -11,8 +11,9 @@ var fs = require("fs");
 //     id: keys.spotify.id,
 //     secret: keys.spotify.secrets
 // });
-// var defMovie = "Mr.Nobody";
-// var defSong = require("The Sign")
+var defMovie = "Mr.Nobody";
+var defSong = "The Sign";
+var defaultBand = "Metallica";
 
 var action = process.argv[2];
 var get = process.argv[3];
@@ -36,10 +37,70 @@ switch (action) {
         break;
 }
 
-// function bands(action) {
+function bands(action) {
+    if (bandname == null) {
+        var bandname = defaultBand;
+        axios.get("https://rest.bandsintown.com/artists/" + bandname + "/events?app_id=codingbootcamp").then(
+            function (response) {
+                result = `
+            Name of the band is: ${defaultBand}
+            Name of the venue is: ${response.data[0].venue.name}
+            Venue location is: ${response.data[0].venue.city}, ${response.data[0].venue.region}, ${response.data[0].venue.country}
+            Date of the event is: ${moment(response.data[0].datetime).format('MM/DD/YYYY')}
+            `
+                console.log(result);
+
+            })
+        .catch(function (error) {
+            if (error.response) {
+                console.log("---------------Data---------------");
+                console.log(error.response.data);
+                console.log("---------------Status---------------");
+                console.log(error.response.status);
+                console.log("---------------Status---------------");
+                console.log(error.response.headers);
+            } else if (error.request) {
+                console.log(error.request);
+            } else {
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
+    } else {
+        var bandname = get.split(" ").join("+");
+        var queryUrl = "https://rest.bandsintown.com/artists/" + bandname + "/events?app_id=codingbootcamp";
+        console.log(queryUrl);
+
+        axios.get("https://rest.bandsintown.com/artists/" + bandname + "/events?app_id=codingbootcamp").then(
+                function (response) {
+                    result = `
+                Name of the venue is: ${response.data[0].venue.name}
+                Venue location is: ${response.data[0].venue.city}, ${response.data[0].venue.region}, ${response.data[0].venue.country}
+                Date of the event is: ${moment(response.data[0].datetime).format('MM/DD/YYYY')}
+                `
+                    console.log(result);
+
+                })
+            .catch(function (error) {
+                if (error.response) {
+                    console.log("---------------Data---------------");
+                    console.log(error.response.data);
+                    console.log("---------------Status---------------");
+                    console.log(error.response.status);
+                    console.log("---------------Status---------------");
+                    console.log(error.response.headers);
+                } else if (error.request) {
+                    console.log(error.request);
+                } else {
+                    console.log("Error", error.message);
+                }
+                console.log(error.config);
+            });
+
+    }
 
 
-// }
+}
 
 // function songs() {
 
@@ -47,10 +108,11 @@ switch (action) {
 
 function movies(get) {
 
-    var queryUrl = "http://www.omdbapi.com/?t=" + get + "&y=&plot=short&apikey=trilogy";
+    var moviename = get.split(" ").join("+");s
+    var queryUrl = "http://www.omdbapi.com/?t=" + moviename + "&y=&plot=short&apikey=trilogy";
     console.log(queryUrl);
 
-    axios.get("http://www.omdbapi.com/?t=" + get + "&y=&plot=short&apikey=trilogy").then(
+    axios.get("http://www.omdbapi.com/?t=" + moviename + "&y=&plot=short&apikey=trilogy").then(
             function (response) {
                 console.log("The movie's Title is: " + response.data.Title);
                 console.log("The movie's Year is: " + response.data.Year);
@@ -71,12 +133,10 @@ function movies(get) {
                 console.log(error.response.headers);
             } else if (error.request) {
                 console.log(error.request);
-
             } else {
                 console.log("Error", error.message);
             }
             console.log(error.config);
-
         });
 
 };
